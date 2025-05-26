5806bd04f0aca734f559d0632880876044c15295
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-05-26 17:48:50 +1200

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { type Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
+function HomePage() {
+  return <Button>Click</Button>
+}
+
+export default HomePage
