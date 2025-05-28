61c80589ef20a6c39b931026c49dcd40d75da351
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-05-28 22:09:52 +1200

    Add sample products data and created product-list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
-import { Button } from '@/components/ui/button'
 import { type Metadata } from 'next'
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
 
 export const metadata: Metadata = {
   title: 'Home',
 }
 
 function HomePage() {
-  return <Button>Click</Button>
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  )
 }
 
 export default HomePage
