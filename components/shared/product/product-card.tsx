42f023bd4cc31aa35aea63f11aa544bbae0d5c3a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-06-03 10:54:56 +1200

    fix eslint

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import ProductPrice from './product-price'
 import { Product } from '@/types'
-import { formatNumberWithDecimal } from '@/lib/utils'
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
