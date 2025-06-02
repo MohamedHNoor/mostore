0fbcfd65ce7f380a0fe8ec11d4b86a4b1c180317
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-06-03 09:16:50 +1200

    Add product-details page

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,74 @@
+import ProductPrice from '@/components/shared/product/product-price'
+import { Card, CardContent } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { getProductBySlug } from '@/lib/actions/product.action'
+import NotFound from '@/app/not-found'
+
+async function ProductDetailPage(props: { params: Promise<{ slug: string }> }) {
+  const { slug } = await props.params
+
+  const product = await getProductBySlug(slug)
+
+  if (!product) return <NotFound />
+
+  return (
+    <section>
+      <div className='grid grid-cols-1 md:grid-cols-5'>
+        {/* Image Column */}
+        <div className='col-span-2'>{/* Add Image Component */}</div>
+
+        {/* Details Column */}
+        <div className='col-span-2 p-5'>
+          <div className='flex flex-col gap-6'>
+            <p>
+              {product.brand} {product.category}
+            </p>
+            <h1 className='h3-bold'>{product.name}</h1>
+            <p>
+              {Number(product.rating)} of {product.numReviews} Reviews
+            </p>
+            <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
+              <ProductPrice
+                value={Number(product.price)}
+                className='w-24 rounded-full bg-yellow-100 text-primary px-5 py-2'
+              />
+            </div>
+          </div>
+          <div className='mt-10'>
+            <p>Description:</p>
+            <p>{product.description}</p>
+          </div>
+        </div>
+        {/* Action Column */}
+        <div>
+          <Card>
+            <CardContent className='p-4'>
+              <div className='mb-2 flex justify-between'>
+                <div>Price</div>
+                <div>
+                  <ProductPrice value={Number(product.price)} />
+                </div>
+              </div>
+              <div className='mb-2 flex justify-between'>
+                <div>Status</div>
+                {product.stock > 0 ? (
+                  <Badge variant='outline'>In stock</Badge>
+                ) : (
+                  <Badge variant='destructive'>Unavailable</Badge>
+                )}
+              </div>
+              {product.stock > 0 && (
+                <div className=' flex-center'>
+                  <Button className='w-full'>Add to cart</Button>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProductDetailPage
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,11 +3,10 @@
 import Image from 'next/image'
 import logo from '@/public/images/logo.svg'
 import { APP_NAME } from '@/lib/constants'
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import { buttonVariants } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 
-function NoFoundPage() {
+function NotFound() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
       <Image
@@ -20,15 +19,16 @@ function NoFoundPage() {
       <div className='p-6 rounded-lg shadow-md w-1/3 text-center'>
         <h1 className='text-3xl font-bold mb-4'>Not Found</h1>
         <p className='text-destructive'>Could not find the requested page.</p>
-        <Link
-          href={'/'}
-          className={`${buttonVariants({ variant: 'outline' })} mt-4 ml-2`}
+        <Button
+          variant='outline'
+          className='mt-4 ml-2'
+          onClick={() => (window.location.href = '/')}
         >
-          Back To Home
-        </Link>
+          Back to home
+        </Button>
       </div>
     </div>
   )
 }
 
-export default NoFoundPage
+export default NotFound
